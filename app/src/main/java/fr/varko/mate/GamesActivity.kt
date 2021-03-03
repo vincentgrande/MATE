f10cace84ad518f256d47b4ad975a8373539a94a
antoine <antoine-haller@outlook.fr>
2021-03-03 10:48:50 +0100

    ajout des jeux selectionnes dans la bdd

--- app/src/main/java/fr/varko/mate/GamesActivity.kt
+++ app/src/main/java/fr/varko/mate/GamesActivity.kt
@@ -1,30 +1,31 @@
 package fr.varko.mate
 
 
-import kotlinx.android.synthetic.main.activity_games.*
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.game_row.*
+import kotlinx.android.synthetic.main.activity_games.*
+
 
 class GamesActivity : AppCompatActivity() {
     private lateinit var database: FirebaseDatabase
+    val gameItems: MutableList<GameItem> = mutableListOf()
+    private val uid = FirebaseAuth.getInstance().uid ?: ""
+    val refUsers = FirebaseDatabase.getInstance().getReference(("/users/$uid"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_games)
         recyclerview_games.adapter = adapter
         database = FirebaseDatabase.getInstance()
         button_done_games.setOnClickListener(){
-            val intent = Intent(this,LatestMessagesActivity::class.java)
+            refUsers.child("playedGames").setValue("$gameItems")
+            val intent = Intent(this, LatestMessagesActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
@@ -33,26 +34,23 @@ class GamesActivity : AppCompatActivity() {
     val adapter = GroupAdapter<ViewHolder>()
 
     private fun ShowGame(){
-        val gameItems: MutableList<GameItem> = mutableListOf()
-        val ref = FirebaseDatabase.getInstance().getReference("/games")
-        ref.addListenerForSingleValueEvent(object:ValueEventListener{
+        val refGames = FirebaseDatabase.getInstance().getReference("/games")
+        refGames.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
                 p0.children.forEach {
-                    Log.d("GamesActivity",it.toString())
+                    Log.d("GamesActivity", it.toString())
                     val game = it.getValue(Game::class.java)
                     adapter.add(GameItem(game))
                     adapter.setOnItemClickListener { game, view ->
                         val gameItem = game as GameItem
-                        if (gameItems.contains(gameItem)){
+                        if (gameItems.contains(gameItem)) {
                             gameItems.remove(gameItem)
-                        }else{
+                        } else {
                             gameItems.add(gameItem)
                         }
                         Log.d("GamesActivity", "$gameItems")
                     }
-
                 }
-
             }
             override fun onCancelled(error: DatabaseError) {
             }
--- app/src/main/java/fr/varko/mate/User.kt
+++ app/src/main/java/fr/varko/mate/User.kt
@@ -4,6 +4,6 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class User (val uid: String, val username: String, val profileImageUrl: String): Parcelable {
-    constructor() : this("","","")
+class User (val uid: String, val username: String, val profileImageUrl: String, val playedGames: String): Parcelable {
+    constructor() : this("","","", "")
 }
